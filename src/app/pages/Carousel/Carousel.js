dd49d260e0c8e50161bfa69ba648f49908d88570
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-06-19 10:36:01 +0500

    Changed slider image

--- src/app/pages/Carousel/Carousel.js
+++ src/app/pages/Carousel/Carousel.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import one from "../../assets/1.jpg";
-import two from "../../assets/2.jpg";
-import three from "../../assets/3.jpg";
+import one from "../../assets/gym.jpg";
+// import two from "../../assets/2.jpg";
+// import three from "../../assets/3.jpg";
 
 const NutritionCarousel = () => {
   return (
@@ -16,14 +16,14 @@ const NutritionCarousel = () => {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
-          <img className="d-block w-100" src={two} alt="Third slide" />
+          <img className="d-block w-100" src={one} alt="Third slide" />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={three} alt="Third slide" />
+          <img className="d-block w-100" src={one} alt="Third slide" />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
